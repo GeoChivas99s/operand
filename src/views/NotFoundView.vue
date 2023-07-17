fc2855303991a5e86d2edc3aceff4369f24e18db
Geovane Chivas <chivasc95@gmail.coms>
2023-07-17 12:09:02 +0100

    feat: add not found page view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="not-found-page">
+        <h1>Page not found 11</h1>
+    </div>
+</template>
+<script>
+</script>
+<style lang="scss">
+.not-found-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+</style>
