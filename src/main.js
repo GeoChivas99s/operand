2793b500655f3b43a60fe044d9c9c526eda3a014
Geovane Chivas <chivasc95@gmail.coms>
2023-07-17 12:08:15 +0100

    fix: fix some bugs in main file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import { store } from './store'
+import { auth } from '@/config/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+import Vuelidate from 'vuelidate'
+import './plugins/fontawesome'
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app
+
+onAuthStateChanged(auth, (user) => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
