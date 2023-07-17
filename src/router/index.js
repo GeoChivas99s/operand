980605cc347360844969b864f82061a44a06c510
Geovane Chivas <chivasc95@gmail.coms>
2023-07-17 12:06:01 +0100

    feat: add mapped routes in project

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { auth } from '@/config/firebase'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/users'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import(/* webpackChunkName: "login" */ '../views/RegisterView.vue')
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: () => import(/* webpackChunkName: "about" */ '../views/UsersView.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/*',
+    name: 'not-found',
+    component: () => import(/* webpackChunkName: "about" */ '../views/NotFoundView.vue')
   }
 ]
 
@@ -24,6 +38,19 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach(async (to, fromRoute, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const user = auth.currentUser
+
+  if (requiresAuth && !user) {
+    next('/login')
+  } else if (user && to.path === '/login') {
+    next('/users')
+  } else {
+    next()
+  }
 })
 
 export default router
