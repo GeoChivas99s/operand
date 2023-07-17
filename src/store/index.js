be2ec5d50bffb47f4f89b83a062b692387a88623
Geovane Chivas <chivasc95@gmail.coms>
2023-07-17 12:03:10 +0100

    feat: add auth store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { authStore } from './auth'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export const store = new Vuex.Store({
   modules: {
+    authStore
   }
 })
